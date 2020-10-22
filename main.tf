c9071c77c4781d850fefb946496e2543f6b0947f
Kimiebi Akah <kimiebi.akah@insight.com>
2020-10-22 11:09:22 -0400

    remove sql db creation

--- main.tf
+++ main.tf
@@ -57,17 +57,17 @@ resource "azurerm_sql_server" "server" {
   location                     = var.location
   version                      = "12.0"
   administrator_login          = "kjakah"
-  administrator_login_password = "ok12346789@!"
+  administrator_login_password = "Ok12346789@!"
 }
 
+# *** DO WE NEED SQL DB OR DW?
+# resource "azurerm_sql_database" "sqldb" {
+#   name                = "${var.prefix}sqldb"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = var.location
+#   server_name         = azurerm_sql_server.server.name
 
-resource "azurerm_sql_database" "sqldb" {
-  name                = "${var.prefix}sqldb"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  server_name         = azurerm_sql_server.server.name
-
-}
+# }
 
 
 
